fa2fac83113edde4e53d4997fba5ce7e1b56f5e3
RSAdrian9 <adrian4rs@gmail.com>
2024-04-09 09:14:26 +0200

    Create: Graphic-Modal

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { App } from '@capacitor/app';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonImg, IonButton, PopoverController, IonBadge, IonIcon, Platform, NavController, AlertController, IonRouterOutlet, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonImg, IonButton, IonBadge, IonIcon, Platform, AlertController, IonRouterOutlet, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, ModalController } from '@ionic/angular/standalone';
 
 
 import { addIcons } from 'ionicons';
 import { funnel } from 'ionicons/icons';
-import { TransferirDatosService } from 'src/app/services/transferir-datos.service';
+import { ModalGraficaComponent } from '../../components/modal-grafica/modal-grafica.component';
+import { DownloadService } from '../../services/download.service';
+import { TransferirDatosService } from '../../services/transferir-datos.service';
 
 @Component({
   selector: 'app-home',
@@ -17,13 +19,15 @@ import { TransferirDatosService } from 'src/app/services/transferir-datos.servic
 })
 export class HomePage implements OnInit {
   public opcionesVentanaPrincipal: any = [
-    { id: 0, nombre: '', icono: '', datos: '', clase: '', tipo: '', posicionTexto: '', mostrar: false }
+    { id: 0, nombre: '', icono: '', datos: '', clase: '', tipo: '', posicionTexto: '', mostrar: false, tipoGrafica: '' }
   ];
   constructor(
     private platform: Platform,
+    private modalCtrl: ModalController,
     private alertCtrl: AlertController,
     private transferirService: TransferirDatosService,
-    private routerOutlet: IonRouterOutlet
+    private routerOutlet: IonRouterOutlet,
+    private downloadService: DownloadService
   ) {
     addIcons({ funnel });
   }
@@ -71,12 +75,31 @@ export class HomePage implements OnInit {
 
   cargarTarjetasInicio() {
     this.opcionesVentanaPrincipal = [
-      { id: 1, nombre: 'Ventas últimos 12 meses', icono: 'assets/icons/sales.svg', datos: '17800', tipo: 'moneda', posicionTexto: 'ion-text-end', clase: 'green', mostrar: true },
-      { id: 2, nombre: 'Compras últimos 12 meses', icono: 'assets/icons/delivery-note.svg', datos: '11600', tipo: 'moneda', posicionTexto: 'ion-text-end', clase: 'orange', mostrar: true },
-      { id: 3, nombre: 'Efectos pendientes de cobro', icono: 'assets/icons/flujo-de-efectivo.svg', datos: '25', tipo: 'texto', posicionTexto: 'ion-text-center', clase: 'blue', mostrar: true },
-      { id: 4, nombre: 'Ratio de cobro', icono: 'assets/icons/bill.svg', datos: '', tipo: 'texto', posicionTexto: 'ion-text-center', clase: 'purple', mostrar: false }
+      { id: 1, nombre: 'Ventas últimos 12 meses', icono: 'assets/imgs/sales.svg', datos: '17800', tipo: 'moneda', posicionTexto: 'ion-text-end', clase: 'green', mostrar: true, tipoGrafica: 'barras' },
+      { id: 2, nombre: 'Compras últimos 12 meses', icono: 'assets/imgs/delivery-note.svg', datos: '11600', tipo: 'moneda', posicionTexto: 'ion-text-end', clase: 'orange', mostrar: true, tipoGrafica: 'barras' },
+      { id: 3, nombre: 'Efectos pendientes de cobro', icono: 'assets/imgs/flujo-de-efectivo.svg', datos: '25', tipo: 'texto', posicionTexto: 'ion-text-center', clase: 'blue', mostrar: true, tipoGrafica: 'sectores' },
+      { id: 4, nombre: 'Ratio de cobro', icono: 'assets/imgs/bill.svg', datos: '', tipo: 'texto', posicionTexto: 'ion-text-center', clase: 'purple', mostrar: false, tipoGrafica: '' }
     ]
   }
+
+  pruebas() {
+    this.downloadService.descargarPaqueteDatos();
+  }
+
+  async abrirModal(id: number, titulo:string, tipoGrafica: string) {
+    console.log('desde home: ' + tipoGrafica);
+    const modal = await this.modalCtrl.create({
+      component: ModalGraficaComponent,
+      componentProps: { id: id, titulo: titulo, tipoGrafica: tipoGrafica }
+
+    });
+    modal.onDidDismiss()
+      .then((data) => {
+
+      });
+    return await modal.present();
+  }
+
 
 
 
--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -1,8 +1,78 @@
 import { Injectable } from '@angular/core';
+import { Network } from '@capacitor/network';
 import { ToastService } from './toast.service';
 import { Directory, Filesystem } from '@capacitor/filesystem';
+import * as JSZip from 'jszip';
+import { DbService } from './db.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DownloadService {}
+export class DownloadService {
+  databaseName = "oficinavirtual.db";
+  endpoint: string = "https://appgp.mjhudesings.com/slim/test-download";
+  nombre: string = "salida.zip";
+
+  constructor(
+    private toastService: ToastService,
+    private dbService: DbService
+
+  ) { }
+
+  public async descargarPaqueteDatos() {
+    try {
+      const status = await Network.getStatus();
+      if (status.connected) {
+        const downloadResult = await Filesystem.downloadFile({
+          path: this.nombre,
+          url: this.endpoint,
+          directory: Directory.External
+        });
+
+        if (downloadResult.path) {
+          const zipData = await Filesystem.readFile({
+            path: this.nombre,
+            directory: Directory.External
+          });
+
+          console.log(zipData);
+          const zip = new JSZip();
+          const zipContent = await zip.loadAsync(zipData.data, { base64: true });
+          // Do something with the unzipped files
+          console.log('Unzipped files:', zipContent);
+
+          zipContent.forEach(async (relativePath: string, zipFile: JSZip.JSZipObject) => {
+            console.log("Write file from zip " + relativePath);
+            await Filesystem.writeFile({
+              path: relativePath,
+              directory: Directory.External,
+              data: await zipFile.async('base64'),
+              recursive: true
+            }).then((data)=>{
+              console.log(data);
+              this.dbService.initializePlugin();
+            });
+          });
+
+
+        } else {
+
+        }
+
+
+      } else {
+        this.toastService.mostrarToast('No tienes conexión a Internet', 1500, 'bottom', 'baseline');
+      }
+    } catch (error) {
+      this.toastService.mostrarToast('Error descargando el paquete:' + error, 1500, 'bottom', 'baseline');
+    }
+  }
+
+  async borrarFicheroZip(){
+    //await Filesystem.deleteFile()
+  }
+  
+
+
+
+}
