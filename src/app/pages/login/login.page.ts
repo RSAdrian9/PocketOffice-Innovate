6e47485b759ddd1a4b6a13587c4e5f6eef816aa4
RSAdrian9 <adrian4rs@gmail.com>
2024-05-02 14:12:10 +0200

    Update: Login (completed)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { navigate } from 'ionicons/icons';
 import { Router } from '@angular/router';
+import { IonCard, IonItem ,IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonCard, IonItem ,IonLabel, IonInput, IonButton]
 })
 export class LoginPage implements OnInit {
+  email: string | undefined;
+  password: string | undefined;
 
-  constructor( private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
