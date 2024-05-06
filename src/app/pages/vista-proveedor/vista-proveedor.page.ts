977526e0cf9daa1aa62deb55da4de36af2f1e342
RSAdrian9 <adrian4rs@gmail.com>
2024-05-06 14:07:29 +0200

    Update: Implementation Vista-Proveedor (completed)

--- src/app/pages/vista-proveedor/vista-proveedor.page.ts
+++ src/app/pages/vista-proveedor/vista-proveedor.page.ts
@@ -1,20 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController, Platform } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { TransferirDatosService } from 'src/app/services/transferir-datos.service';
+import { DbService } from 'src/app/services/db.service';
+import { UtilitiesService } from 'src/app/services/utilities.service';
+import { addIcons } from 'ionicons';
+import { callOutline, mailOutline, globeOutline } from 'ionicons/icons';
+import { IonItem, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonImg, IonList, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-vista-proveedor',
   templateUrl: './vista-proveedor.page.html',
   styleUrls: ['./vista-proveedor.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonImg, IonList, IonIcon, IonItem]
 })
 export class VistaProveedorPage implements OnInit {
+  public codigoProveedor!: string;
+  public proveedor!: any;
+  private activatedRoute = inject(ActivatedRoute);
 
-  constructor() { }
+  constructor(
+    private platform: Platform,
+    private navC: NavController,
+    private transferirService: TransferirDatosService,
+    private dbService: DbService,
+    private utils: UtilitiesService
+  ) {
+    addIcons({ callOutline, mailOutline, globeOutline });
+
+  }
 
   ngOnInit() {
+    this.codigoProveedor = this.activatedRoute.snapshot.paramMap.get('codigo') as string;
+    this.cargarDatosProveedor();
+    this.pageController('/vista-proveedor');
+  }
+
+  ionViewDidEnter() {
+    this.codigoProveedor = this.activatedRoute.snapshot.paramMap.get('codigo') as string;
+    this.pageController('/vista-proveedor');
+  }
+
+  private cargarDatosProveedor() {
+    this.dbService.getProveedor(this.codigoProveedor).then((proveedor) => {
+      this.proveedor = proveedor;
+    });
+  }
+
+  pageController(route: string) {
+    this.transferirService.sendObjectSource({ ruta: route });
+    this.transferirService.sendObjectSource({ codigo: this.codigoProveedor });
+
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      this.navC.navigateBack('/proveedores');
+      this.transferirService.sendObjectSource({ ruta: '/proveedores' });
+    });
+  }
+
+  valid(e: String) {
+    return this.utils.valid(e)
+  }
+
+  callNumber(number: string) {
+    window.open(`tel:${number}`, '_system');
+  }
+
+  mailto(email: string) {
+    window.open(`mailto:${email}`, '_system');
+  }
+
+  web(web: string) {
+    window.open(`http://${web}`, '_system');
   }
 
 }
